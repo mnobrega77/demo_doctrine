59bebf2396f7452831f86b311275dc00ae0f6959
@mnobrega <mnobrega77@yahoo.com>
2023-06-16 09:50:30 +0200

    git first commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Auteur;
+use App\Entity\Livre;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -16,8 +18,37 @@ class AppFixtures extends Fixture
 
         $faker = Factory::create('fr_FR');
 
+// on crée 4 auteurs avec noms et prénoms "aléatoires" en français
+        $auteurs = [];
 
+        for ($i = 0; $i < 10; $i++) {
+            $auteur = new Auteur();
+            $auteur->setNom($faker->lastName);
+            $auteur->setPrenom($faker->firstName);
+
+            $manager->persist($auteur);
+            $auteurs[] = $auteur;
+
+        }
+
+
+        // nouvelle boucle pour créer des livres
+
+
+
+        for ($i = 0; $i < 30; $i++) {
+            $livre = new Livre();
+            $livre->setTitre($faker->sentence($nbWords = 4, $variableNbWords = true));
+            $livre->setEditeur($faker->word);
+            $livre->setDescription($faker->sentence(10, true));
+            $livre->setCouverture('http://placeimg.com/640/480/nature');
+            $livre->setDateParution(new \DateTime());
+            $livre->setAuteur($auteurs[$i % 9]);
+
+            $manager->persist($livre);
+        }
 
         $manager->flush();
     }
+
 }
---
+++ src/Entity/Livre.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivreRepository::class)]
+class Livre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column()]
+    private ?\DateTime $dateParution = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $editeur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $couverture = null;
+
+    #[ORM\ManyToOne(inversedBy: 'livres')]
+    private ?Auteur $auteur = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDateParution(): ?\DateTimeInterface
+    {
+        return $this->dateParution;
+    }
+
+    public function setDateParution(\DateTimeInterface $dateParution): static
+    {
+        $this->dateParution = $dateParution;
+
+        return $this;
+    }
+
+    public function getEditeur(): ?string
+    {
+        return $this->editeur;
+    }
+
+    public function setEditeur(string $editeur): static
+    {
+        $this->editeur = $editeur;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCouverture(): ?string
+    {
+        return $this->couverture;
+    }
+
+    public function setCouverture(string $couverture): static
+    {
+        $this->couverture = $couverture;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?Auteur
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?Auteur $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+
+}
